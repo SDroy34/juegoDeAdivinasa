a06aafe7726ab554f2a6de6199739f28ddd26a55
Asgard Juarez Campos <asgardprogramacion@gmail.com>
2024-12-21 22:58:11 -0600

    Se agregaron objetos el objeto Condicíon

---
+++ juegoDeAdivinasa/Class1.cs
@@ -0,0 +1,35 @@
+﻿using juegodeadivinasa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace juegoDeAdivinasa
+{
+    public class Condiciones
+    {
+        public int numerosUsuarios { get; set; }
+        public int numeroRandom { get; set; }
+        public bool conversionExitosa { get; set; }
+        public void CondicionMasBajoAlto(int numeroUsuario, int numeroRandom)
+        {
+            if (numeroUsuario > numeroRandom)
+            {
+                Console.WriteLine("Mas bajo");
+            }
+            else
+            {
+                Console.WriteLine("Mas alto");
+            }
+
+        }
+        public void CondicionNumeroUsuario(bool conversionExitosa)
+        {
+            if (conversionExitosa == false)
+            {
+                Console.WriteLine("Pon un numero valido");
+            }
+        }
+    }
+}
--- juegoDeAdivinasa/Program.cs
+++ juegoDeAdivinasa/Program.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using juegoDeAdivinasa;
+using System;
 
-namespace juegoDeAdivinasa
+//2.Juego de adivinanzas(número secreto)
+//Descripción: Un pequeño juego en el que los jugadores
+//intentan adivinar un número secreto con pistas (más alto, más bajo).
+//Características:
+//Contador de intentos.
+//Mensajes dinámicos para guiar al jugador.
+//Opción para jugar nuevamente.
+//Tecnologías:
+//Consola para la interfaz básica o WinForms para algo más visual.
+//Roles:
+//Uno puede diseñar la lógica del juego.
+//Otro puede trabajar en el diseño y las interacciones.
+//Un tercero puede agregar características adicionales como dificultad o estadísticas.
+
+
+namespace juegodeadivinasa
 {
-    class juegoAdivinasaMain
+    class JuegoAdivinasaMain
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Este es un juego de adivinar el numero");
+            //Se crea un numero aleatorio
+            Random numRando = new Random();
+            int numeroAleatorio = numRando.Next(100);
+            int numUsuario;
+            int intentos = 0; 
+            //Se crea un objeto para mandar a llamar nuestras condiciones
+            Condiciones condiciones = new Condiciones();
+            condiciones.numeroRandom = numeroAleatorio;
+            do
+            {
+                Console.WriteLine("Pon un numero del uno al cien.");
+                var numString = Console.ReadLine();
+                //Nos aseguramos de que el numero ingresado se entero
+                bool converExit = int.TryParse(numString, out numUsuario);
+                //Comprobamos que el numero sea entero
+                condiciones.CondicionNumeroUsuario(converExit);
+                if (converExit == true)
+                {
+                    //Damos pistas al usuario
+                    condiciones.CondicionMasBajoAlto(numUsuario, numeroAleatorio);
+                }
+                
+                intentos++;
+            } while (numUsuario != numeroAleatorio);
+            Console.WriteLine($"Felicidades encontrate el numeor aleatorio {numeroAleatorio} en el intento {intentos}");
 
         }
     }
+
 }
