8273ec1816bacb6eea661af680c48ce63432fb1f
Asgard Juarez Campos <asgardprogramacion@gmail.com>
2024-12-22 20:38:27 -0600

    Se agreag un Swich de dificultades

--- juegoDeAdivinasa/Program.cs
+++ juegoDeAdivinasa/Program.cs
@@ -1,5 +1,6 @@
 ﻿using juegoDeAdivinasa;
 using System;
+using System.Numerics;
 
 //2.Juego de adivinanzas(número secreto)
 //Descripción: Un pequeño juego en el que los jugadores
@@ -22,34 +23,78 @@ namespace juegodeadivinasa
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Este es un juego de adivinar el numero");
             //Se crea un numero aleatorio
             Random numRando = new Random();
-            int numeroAleatorio = numRando.Next(100);
             int numUsuario;
-            int intentos = 0; 
-            //Se crea un objeto para mandar a llamar nuestras condiciones
+            int intentos = 0, opc;
             Condiciones condiciones = new Condiciones();
-            condiciones.numeroRandom = numeroAleatorio;
-            do
+            //Se muestra la lista de opciones
+            Console.WriteLine("Este es un juego de adivinar el numero");
+            Console.WriteLine("Elije un nivel\n" +
+                "1. Facil\n" +
+                "2. Medio\n" +
+                "3. Dificil");
+            var opcString = Console.ReadLine();
+            bool converExit2 = int.TryParse(opcString, out opc);
+            condiciones.CondicionNumeroUsuario(converExit2);
+            if (converExit2 == true)
             {
-                Console.WriteLine("Pon un numero del uno al cien.");
-                var numString = Console.ReadLine();
-                //Nos aseguramos de que el numero ingresado se entero
-                bool converExit = int.TryParse(numString, out numUsuario);
-                //Comprobamos que el numero sea entero
-                condiciones.CondicionNumeroUsuario(converExit);
-                if (converExit == true)
+                switch (opc)
                 {
-                    //Damos pistas al usuario
-                    condiciones.CondicionMasBajoAlto(numUsuario, numeroAleatorio);
+                    case 1:
+                        int numeroFacil = numRando.Next(10);
+                        Console.WriteLine("Pon un numero del 1 al 10");
+                        do
+                        {
+                            var numString = Console.ReadLine();
+                            bool converExitosa = int.TryParse(numString, out numUsuario);
+                            condiciones.CondicionNumeroUsuario(converExitosa);
+                            if (converExitosa == true)
+                            {
+                                condiciones.CondicionMasBajoAlto(numUsuario, numeroFacil);
+                            }
+                            intentos++;
+                        } while (numeroFacil != numUsuario);
+                        Console.WriteLine($"Felicidades encontrate el numeor aleatorio {numeroFacil} en el intento {intentos}");
+                        break;
+                    case 2:
+                        int numeroMedio = numRando.Next(100);
+                        Console.WriteLine("Pon un numero del 1 al 100");
+                        do
+                        {
+                            var numString = Console.ReadLine();
+                            bool converExitosa = int.TryParse(numString, out numUsuario);
+                            condiciones.CondicionNumeroUsuario(converExitosa);
+                            if (converExitosa == true)
+                            {
+                            condiciones.CondicionMasBajoAlto(numUsuario, numeroMedio);
+                            }
+                            intentos++;
+                        } while (numeroMedio != numUsuario);
+                        Console.WriteLine($"Felicidades encontrate el numeor aleatorio {numeroMedio} en el intento {intentos}");
+                        break;
+                    case 3:
+                        int numeroDificil = numRando.Next(1000);
+                        Console.WriteLine("Pon un numero del 1 al 1000");
+                        do
+                        {
+                            var numString = Console.ReadLine();
+                            bool converExitosa = int.TryParse(numString, out numUsuario);
+                            condiciones.CondicionNumeroUsuario(converExitosa);
+                            if (converExitosa == true)
+                            {
+                                condiciones.CondicionMasBajoAlto(numUsuario, numeroDificil);
+                            }
+                            intentos++;
+                        } while (numeroDificil != numUsuario);
+                        Console.WriteLine($"Felicidades encontrate el numeor aleatorio {numeroDificil} en el intento {intentos}");
+                        break;
+                    default:
+                        Console.WriteLine("Elije una opcion valida");
+                        break;
                 }
-                
-                intentos++;
-            } while (numUsuario != numeroAleatorio);
-            Console.WriteLine($"Felicidades encontrate el numeor aleatorio {numeroAleatorio} en el intento {intentos}");
 
+            }
         }
     }
-
 }
